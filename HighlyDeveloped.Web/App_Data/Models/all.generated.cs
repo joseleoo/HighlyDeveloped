3950f1df8b715cd8403d826b6932e22d067d8b84
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2024-01-10 12:09:16 -0500

    38. Adding Twitter - the Core project

--- HighlyDeveloped.Web/App_Data/Models/all.generated.cs
+++ HighlyDeveloped.Web/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "3642095bcae65775")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
