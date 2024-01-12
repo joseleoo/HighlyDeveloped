9044f0ea140dc1a45530f5301b42e062038abefe
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2024-01-12 09:13:10 -0500

    40. Adding Twitter - the API Call
    it didn't work

---
+++ HighlyDevelop.Core/ViewModel/TwitterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighlyDevelop.Core.ViewModel
+{
+    /// <summary>
+    /// This will hold the twitter tweet data for rendering the lastest tweets
+    /// </summary>
+    public class TwitterViewModel
+    {
+        public string TwitterHandle { get; set; }
+        public bool Error { get; set; }
+        public string Json { get; set; }
+        public string Message { get; set; }
+    }
+}
